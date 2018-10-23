b38d88c6b62b15b6321659c1754371131c505c8e
Emiel van den Brink <emielvdbrink@gmail.com>
2018-10-23 22:08:48 +0200

    added a moveable player

--- behaviour.js
+++ behaviour.js
@@ -1,5 +1,6 @@
 var rows = 10;
 var columns = 20;
+var playerPosY, playerPosX;
 var grid;
 
 function generateLayout() {
@@ -8,43 +9,84 @@ function generateLayout() {
     for (y = 0; y < rows; y++) {
         grid[y] = [];
         for (x = 0; x < columns; x++) {
-            if (y === 0 || y === rows - 1 || x === 0 || x === columns - 1) {
+            if (y === 0 || y === rows - 1 || x === 0 || x === columns - 1)
                 grid[y][x] = '#';
-            }
-            else {
+            else
                 grid[y][x] = '.';
-            }
-
         }
     }
 
     return grid;
 }
 
-function drawGrid() {
-    var textRows = [];
-    for (i = 0; i < rows; i++) {
-        textRows[i] = grid[i].join('');
+function setPlayerPos() {
+    playerPosY = 1;
+    playerPosX = 1;
+}
+
+function movePlayer(direction) {
+    switch (direction) {
+        case 'UP':
+            if (isTraverseable(playerPosY - 1, playerPosX))
+                playerPosY--;
+            break;
+        case 'DOWN':
+            if (isTraverseable(playerPosY + 1, playerPosX))
+                playerPosY++;
+            break;
+        case 'LEFT':
+            if (isTraverseable(playerPosY, playerPosX - 1))
+                playerPosX--;
+            break;
+        case 'RIGHT':
+            if (isTraverseable(playerPosY, playerPosX + 1))
+                playerPosX++;
+            break;
     }
-    document.getElementById("PlayField").innerHTML = textRows.join('<br>');
+
+    drawDisplay();
+}
+
+function isTraverseable(y, x) {
+    if (y < 0 || y > rows || x < 0 || x > columns)
+        return false;
+
+    return grid[y][x] === '.';
+}
+
+function drawDisplay() {
+    var display = [];
+
+    for (y = 0; y < rows; y++) {
+        display[y] = [];
+        for (x = 0; x < columns; x++) {
+            if (playerPosY === y && playerPosX === x)
+                display[y][x] = '@';
+            else
+                display[y][x] = grid[y][x];
+        }
+    }
+
+    document.getElementById("PlayField").innerHTML = display.map(arr => arr.join('')).join('<br>');
 }
 
 window.onload = function () {
     grid = generateLayout();
-    drawGrid();
+    setPlayerPos();
+    drawDisplay();
     document.onkeydown = function (e) {
         switch (String.fromCharCode(e.keyCode)) {
             case 'W':
-                console.log('MOVE UP');
+                movePlayer('UP');
                 break;
             case 'A':
-                console.log('MOVE LEFT');
+                movePlayer('LEFT');
                 break;
             case 'S':
-                console.log('MOVE DOWN');
+                movePlayer('DOWN');
                 break;
             case 'D':
-                console.log('MOVE RIGHT');
+                movePlayer('RIGHT');
                 break;
         }
     };
